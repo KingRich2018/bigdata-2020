a54de4cc802568fa269d4d82b5d8b1a12f1eddaa
KingRich2018 <1719873656@qq.com>
2020-02-21 22:31:51 +0800

    主题 枚举

--- gmall-communal/src/main/java/com/wang/constant/GmallConstants.java
+++ gmall-communal/src/main/java/com/wang/constant/GmallConstants.java
@@ -7,6 +7,7 @@ public class GmallConstants {
     public static final String KAFKA_TOPIC_EVENT="GMALL_EVENT";
 
     public static final String KAFKA_TOPIC_NEW_ORDER="GMALL_NEW_ORDER";
+    // GMV主题
     public static final String KAFKA_TOPIC_ORDER_DETAIL="GMALL_ORDER_DETAIL";
 
     public static final String ES_INDEX_DAU="gmall2019_dau";
